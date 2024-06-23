bf076aee569a23c66727781ef0d27c997ae4b173
digitribesthlm <patrik@makeablesthlm.se>
2024-06-23 11:22:10 +0200

    Nr p

--- pages/index.js
+++ pages/index.js
@@ -168,6 +168,8 @@ const Home = () => {
                             <option value={1}>1</option>
                             <option value={2}>2</option>
                             <option value={3}>3</option>
+                            <option value={4}>4</option>
+                            <option value={5}>5</option>
                         </select>
                     </div>
                     <PuttForm hole={currentHole} numPutts={numPutts} formData={formData} handleFormDataChange={handleFormDataChange} />
