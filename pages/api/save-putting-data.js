2387f4147495dfd88efeb1fb74384ce394c33e7a
digitribesthlm <patrik@makeablesthlm.se>
2024-06-26 17:37:13 +0200

    Update save-putting-data.js

--- pages/api/save-putting-data.js
+++ pages/api/save-putting-data.js
@@ -1,5 +1,6 @@
+
+// pages/api/save-putting-data.js
 import { connectToDatabase } from '../../lib/mongodb';
-import jwt from 'jsonwebtoken';
 
 export default async (req, res) => {
   if (req.method === 'POST') {
@@ -13,32 +14,12 @@ export default async (req, res) => {
       const { db } = await connectToDatabase();
       const collectionName = process.env.MONGODB_COLLECTION;
       const collection = db.collection(collectionName);
-      
-      // Get the token from the request headers
-      const token = req.headers.authorization?.split(' ')[1];
 
-      if (!token) {
-        return res.status(401).json({ error: 'Unauthorized' });
-      }
-
-      // Verify and decode the token
-      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-      const userId = decodedToken.userId;
-
-      // Find the user in the database using the userId
-      const usersCollection = db.collection('users');
-      const user = await usersCollection.findOne({ _id: userId });
-
-      if (!user) {
-        return res.status(401).json({ error: 'Unauthorized' });
-      }
-      
       const newPuttingData = {
         course,
         holes,
         statistics,
         createdAt: new Date(),
-        user: user.email,
       };
 
       const result = await collection.insertOne(newPuttingData);
@@ -56,3 +37,4 @@ export default async (req, res) => {
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
+
