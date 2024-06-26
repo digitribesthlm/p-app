3d1a14f38b2c48b3d60558777294b5e7e8547ae5
digitribesthlm <patrik@makeablesthlm.se>
2024-06-26 17:04:46 +0200

    Simplified the save-putting-data API endpoint by removing user authentication and using a generic user identifier.

--- pages/api/save-putting-data.js
+++ pages/api/save-putting-data.js
@@ -1,54 +1,36 @@
 import { connectToDatabase } from '../../lib/mongodb';
-import jwt from 'jsonwebtoken';
-import { ObjectId } from 'mongodb';
 
 export default async (req, res) => {
   if (req.method === 'POST') {
     const { course, holes, statistics } = req.body;
 
     if (!course || !holes || !statistics) {
-      return res.status(400).json({ error: 'Missing required fields!' });
+      return res.status(400).json({ error: 'Missing required fields' });
     }
 
     try {
       const { db } = await connectToDatabase();
-      const usersCollection = db.collection('users');
-      const puttingDataCollection = db.collection('putting');
-
-      const token = req.headers.authorization?.split(' ')[1];
-      if (!token) {
-        return res.status(401).json({ error: 'No token provided' });
-      }
-
-      let decodedToken;
-      try {
-        decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-      } catch (error) {
-        return res.status(401).json({ error: 'Invalid token' });
-      }
-
-      const userId = decodedToken.userId;
-
-      const user = await usersCollection.findOne({ _id: new ObjectId(userId) });
-      if (!user) {
-        return res.status(404).json({ error: 'User not found' });
-      }
-
+      const collectionName = process.env.MONGODB_COLLECTION;
+      const collection = db.collection(collectionName);
+      
+      const user = 'test-user2'
+      
       const newPuttingData = {
         course,
         holes,
         statistics,
-        userId: new ObjectId(userId),
-        user: user.email,
         createdAt: new Date(),
+        user,
       };
 
-      const result = await puttingDataCollection.insertOne(newPuttingData);
-      const insertedDocument = await puttingDataCollection.findOne({ _id: result.insertedId });
+      const result = await collection.insertOne(newPuttingData);
+
+      // Use insertedId instead of ops
+      const insertedDocument = await collection.findOne({ _id: result.insertedId });
 
       return res.status(201).json({ message: 'Putting data saved', data: insertedDocument });
     } catch (error) {
-      console.error('Error saving putting data:', error);
+      console.error('Error saving putting data:', error.message, error.stack);
       return res.status(500).json({ error: 'Internal Server Error' });
     }
   } else {
