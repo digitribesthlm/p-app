e736b3eeb2dfc485d8bd8088c8b12b70d7c73b4f
digitribesthlm <patrik@makeablesthlm.se>
2024-06-28 09:03:19 +0200

    Added ThemeSelector component.

---
+++ components/ThemeSelector.js
@@ -0,0 +1,64 @@
+import { useEffect } from 'react';
+
+const ThemeSelector = () => {
+  const themes = [
+    "light",
+    "dark",
+    "cupcake",
+    "bumblebee",
+    "emerald",
+    "corporate",
+    "synthwave",
+    "retro",
+    "cyberpunk",
+    "valentine",
+    "halloween",
+    "garden",
+    "forest",
+    "aqua",
+    "lofi",
+    "pastel",
+    "fantasy",
+    "wireframe",
+    "black",
+    "luxury",
+    "dracula",
+    "cmyk",
+    "autumn",
+    "business",
+    "acid",
+    "lemonade",
+    "night",
+    "coffee",
+    "winter",
+    "dim",
+    "nord",
+    "sunset",
+  ];
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', themes[0]);
+  }, []);
+
+  const handleThemeChange = (theme) => {
+    document.documentElement.setAttribute('data-theme', theme);
+    document.body.setAttribute('data-theme', theme);
+  };
+
+  return (
+    <div>
+      <select onChange={(e) => handleThemeChange(e.target.value)}>
+        {themes.map((theme) => (
+          <option key={theme} value={theme}>
+            {theme}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default ThemeSelector;
+
+
+
