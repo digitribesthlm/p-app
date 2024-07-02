19c0ad907c96e0ad13c4da311c2dc05e78a34898
digitribesthlm <patrik@makeablesthlm.se>
2024-07-02 16:55:37 +0200

    Connected to MongoDB and fetched data from 'putting' and 'putting_stats' collections.

--- pages/api/fetch-putting-data.js
+++ pages/api/fetch-putting-data.js
@@ -13,3 +13,18 @@ const fetchPuttingData = async (req, res) => {
 };
 
 export default fetchPuttingData;
+import { MongoClient } from 'mongodb';
+
+const fetchPuttingData = async (req, res) => {
+  const client = await MongoClient.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const db = client.db();
+  const puttingData = await db.collection('putting').find({}).toArray();
+  const puttingStats = await db.collection('putting_stats').find({}).toArray();
+  client.close();
+  res.status(200).json({ puttingData, puttingStats });
+};
+
+export default fetchPuttingData;
