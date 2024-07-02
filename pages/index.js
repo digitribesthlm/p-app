e575004fc5731728f7b445bbcf9794d83137f2df
digitribesthlm <patrik@makeablesthlm.se>
2024-07-02 12:28:58 +0200

    test

--- pages/index.js
+++ pages/index.js
@@ -48,7 +48,7 @@ const Home = () => {
     }, []);
 
     useEffect(() => {
-        // Calculate statistics and save formData to session-based cookie whenever formData changes
+        // Calculate statistics and save formData to session-based cookie whenever formData changes.
         let puttCount = 0;
         let shortCount = 0;
         let lastPuttMeters = 0;
