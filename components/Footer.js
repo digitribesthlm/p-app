d320d1983147bd0b3473eea82ff48d4d9b86885b
digitribesthlm <patrik@makeablesthlm.se>
2024-09-07 16:14:58 +0200

    Footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className="bg-gray-800 text-white p-4 text-center fixed bottom-0 w-full">
-            <p>&copy; {new Date().getFullYear()} Putting App. All rights reserved.</p>
+            <p>&copy; {new Date().getFullYear()} Putting App. All rights reserved..</p>
         </footer>
     );
 };
