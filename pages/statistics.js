4350e4acde957785740ef15d8c478a58cfd9d1f1
digitribesthlm <patrik@makeablesthlm.se>
2024-09-07 16:12:58 +0200

    Test

--- pages/statistics.js
+++ pages/statistics.js
@@ -97,7 +97,7 @@ const Statistics = () => {
                                                                                                                       
    return (                                                                                                           
      <div className="container mx-auto p-4">                                                                          
-       <h1 className="text-2xl font-bold mb-4">Putting Statistics!!</h1>
+       <h1 className="text-2xl font-bold mb-4">Putting Statistics 3</h1>
        <ThemeSelector />
        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">                                         
          {puttingData.map((round) => {                                                                                
